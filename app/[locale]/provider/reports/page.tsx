3e12d1f5d4c6ddc15f94bed3c844eba3ea6ede79
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2024-03-20 22:39:54 +0500

    added sidebar navigation to provider

---
+++ app/[locale]/provider/reports/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import HeaderInfoItem from '@/components/header/HeaderInfoItem'
+import PerformanceChart from '@/components/header/PerformanceChart'
+import ReportWidget from '@/components/header/ReportWidget'
+import PageHeader from '@/components/ui/PageHeader'
+import { messages } from '@/constants/constants'
+import { ColorsEnum, ReportTypesEnum } from '@/constants/enums'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Report = () => {
+    const { t } = useTranslation()
+
+    return (
+        <div className="flex flex-col gap-4 h-full w-full p-5 pb-0 overflow-auto scrollbar">
+            <PageHeader title={t(messages.REPORTS)}
+                description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis asperiores, aperiam ipsum corrupti minus recusandae exercitationem dolorum temporibus esse at officia iure in hic consequatur ea nisi placeat praesentium officiis."
+            >
+            </PageHeader>
+
+            <div className="w-full grid grid-cols-1 md:grid-cols-4 md:grid-row-1 gap-3">
+                <PerformanceChart />
+
+
+                <div className="bg-indigo-800/5 rounded-sm col-span-1 grid grid-cols-1 grid-rows-5 gap-3 p-3">
+                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Blue} heading={43} className="bg-white" />
+                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={100} percentage={67} showIcon hasGraph className="bg-white" />
+                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Red} heading={342} className="bg-white" />
+                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Yellow} heading={32} className="bg-white" />
+                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={75} percentage={96} showIcon className="bg-white" />
+                </div>
+
+            </div>
+            <div className="w-full grid grid-cols-1 md:grid-cols-3 md:grid-row-1 gap-3">
+                {Object.values(ReportTypesEnum).map(report => (
+                    <ReportWidget type={report} key={report} />
+                ))}
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Report
---
+++ app/[locale]/provider/subscription/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import PageHeader from '@/components/ui/PageHeader'
+import { messages } from '@/constants/constants'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Subscriptions = () => {
+    const { t } = useTranslation()
+
+    return (
+        <div className="flex flex-col gap-4 h-full w-full p-5 pb-0 overflow-auto scrollbar">
+            <PageHeader title={t(messages.REPORTS)}
+                description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis asperiores, aperiam ipsum corrupti minus recusandae exercitationem dolorum temporibus esse at officia iure in hic consequatur ea nisi placeat praesentium officiis."
+            >
+            </PageHeader>
+
+
+
+
+        </div>
+    )
+}
+
+export default Subscriptions
