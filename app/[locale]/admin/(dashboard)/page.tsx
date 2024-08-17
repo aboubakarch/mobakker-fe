72a5dd66f063ce326d3699873a248f68d2b2348f
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2024-08-17 23:00:07 +0500

    added some stats

--- app/[locale]/admin/(dashboard)/page.tsx
+++ app/[locale]/admin/(dashboard)/page.tsx
@@ -7,13 +7,46 @@ import NotificationHandler from "@/components/notificationHandler/NotificationHa
 import { Button } from "@/components/ui";
 import { messages } from "@/constants/constants";
 import { ColorsEnum } from "@/constants/enums";
+import APIService from "@/services/api";
 import { ExportIcon } from "@/svgs";
+import { useEffect, useState } from "react";
 // import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-
+interface IAppointmentState {
+  COMPLETED?: number;
+  PENDING?: number;
+  STARTED?: number;
+  REJECTED?: number;
+  CANCELED?: number;
+}
 
 export default function Home() {
   const { t } = useTranslation()
+  const [totalAppointments, setTotalAppointments] = useState<IAppointmentState | undefined | null>(undefined)
+  const [totalAppointmentsCount, setTotalAppointmentsCount] = useState(0)
+  const getTotalAppointments = async () => {
+    try {
+      let param: any = {}
+
+      const data = await APIService.getInstance().getTotalAppointments(param)
+      setTotalAppointments({
+        CANCELED: data.CANCELED || 0,
+        COMPLETED: data.COMPLETED || 0,
+        PENDING: data.PENDING || 0,
+        REJECTED: data.REJECTED || 0,
+        STARTED: data.STARTED || 0,
+      })
+      setTotalAppointmentsCount(Object.values(data).reduce((acc: number, value: any) => acc + (value || 0), 0))
+
+    } catch (error) {
+      setTotalAppointments(null)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getTotalAppointments()
+  }, [])
+
   // useEffect(() => {
   //   document.cookie = "username=John Doe";
   //   console.log(document.cookie)
@@ -39,11 +72,11 @@ export default function Home() {
 
 
         <div className="bg-indigo-800/5 rounded-sm col-span-1 grid grid-cols-1 grid-rows-5 gap-3 p-3">
-          <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Blue} heading={43} className="bg-white" />
-          <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={100} percentage={67} showIcon hasGraph className="bg-white" />
-          <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Red} heading={342} className="bg-white" />
-          <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Yellow} heading={32} className="bg-white" />
-          <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={75} percentage={96} showIcon className="bg-white" />
+          <HeaderInfoItem title={t(messages.TOTAL_REQ)} color={ColorsEnum.Blue} heading={totalAppointmentsCount} className="bg-white" />
+          <HeaderInfoItem title={t(messages.COMPLETE)} color={ColorsEnum.Green} heading={totalAppointments?.COMPLETED || 0} className="bg-white" />
+          <HeaderInfoItem title={t("Pending")} color={ColorsEnum.Yellow} heading={totalAppointments?.PENDING || 0} className="bg-white" />
+          <HeaderInfoItem title={t(messages.CANCELLED)} color={ColorsEnum.Red} heading={totalAppointments?.CANCELED || 0} className="bg-white" />
+          <HeaderInfoItem title={t("Rejected")} color={ColorsEnum.Red} heading={totalAppointments?.REJECTED || 0} className="bg-white" />
         </div>
 
       </div>
--- app/[locale]/admin/reports/page.tsx
+++ app/[locale]/admin/reports/page.tsx
@@ -6,11 +6,42 @@ import ReportWidget from '@/components/header/ReportWidget'
 import PageHeader from '@/components/ui/PageHeader'
 import { messages } from '@/constants/constants'
 import { ColorsEnum, ReportTypesEnum } from '@/constants/enums'
-import React from 'react'
+import APIService from '@/services/api'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-
+interface IAppointmentState {
+    COMPLETED?: number;
+    PENDING?: number;
+    STARTED?: number;
+    REJECTED?: number;
+    CANCELED?: number;
+}
 const Report = () => {
     const { t } = useTranslation()
+    const [totalAppointments, setTotalAppointments] = useState<IAppointmentState | undefined | null>(undefined)
+    const [totalAppointmentsCount, setTotalAppointmentsCount] = useState(0)
+    const getTotalAppointments = async () => {
+        try {
+            let param: any = {}
+
+            const data = await APIService.getInstance().getTotalAppointments(param)
+            setTotalAppointments({
+                CANCELED: data.CANCELED || 0,
+                COMPLETED: data.COMPLETED || 0,
+                PENDING: data.PENDING || 0,
+                REJECTED: data.REJECTED || 0,
+                STARTED: data.STARTED || 0,
+            })
+            setTotalAppointmentsCount(Object.values(data).reduce((acc: number, value: any) => acc + (value || 0), 0))
+
+        } catch (error) {
+            setTotalAppointments(null)
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getTotalAppointments()
+    }, [])
 
     return (
         <div className="flex flex-col gap-4 h-full w-full p-5 pb-0 overflow-auto scrollbar">
@@ -24,11 +55,11 @@ const Report = () => {
 
 
                 <div className="bg-indigo-800/5 rounded-sm col-span-1 grid grid-cols-1 grid-rows-5 gap-3 p-3">
-                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Blue} heading={43} className="bg-white" />
-                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={100} percentage={67} showIcon hasGraph className="bg-white" />
-                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Red} heading={342} className="bg-white" />
-                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Yellow} heading={32} className="bg-white" />
-                    <HeaderInfoItem title="Lorem Ipsum" color={ColorsEnum.Green} heading={75} percentage={96} showIcon className="bg-white" />
+                    <HeaderInfoItem title={t(messages.TOTAL_REQ)} color={ColorsEnum.Blue} heading={totalAppointmentsCount} className="bg-white" />
+                    <HeaderInfoItem title={t(messages.COMPLETE)} color={ColorsEnum.Green} heading={totalAppointments?.COMPLETED || 0} className="bg-white" />
+                    <HeaderInfoItem title={t("Pending")} color={ColorsEnum.Yellow} heading={totalAppointments?.PENDING || 0} className="bg-white" />
+                    <HeaderInfoItem title={t(messages.CANCELLED)} color={ColorsEnum.Red} heading={totalAppointments?.CANCELED || 0} className="bg-white" />
+                    <HeaderInfoItem title={t("Rejected")} color={ColorsEnum.Red} heading={totalAppointments?.REJECTED || 0} className="bg-white" />
                 </div>
 
             </div>
